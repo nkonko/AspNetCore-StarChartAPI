9095d9e04bc46eec6948ce36d75aabdd03f47faf
nicolas azzara <nicoazzara@hotmail.com>
2019-07-31 23:57:25 -0300

    added controller dbcontext

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+    }
+}
